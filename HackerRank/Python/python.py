c950f2191374f1926fc396aced39c72ca5bcd1ef
Muhammad Asif <asif88383@gmail.com>
2022-05-05 13:20:32 +0500

    first commit

--- HackerRank/Python/python.py
+++ HackerRank/Python/python.py
@@ -21,8 +21,20 @@ def is_leap(year):
     leap = False
     
     # Write your logic here
-    if(year%4 == 0):
-        leap=True
+   def is_leap(year):
+    leap = False
     
+    # Write your logic here
+    if(year%4 == 0):
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                leap=True
+                return leap
+            leap=False
+            return leap
+        leap=True
+        return leap    
     return leap
+
+
 
