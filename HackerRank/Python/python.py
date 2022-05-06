fd06dad5593645af84b7895fb36ec0228ded9494
Muhammad Asif <asif88383@gmail.com>
2022-05-06 15:05:22 +0500

    first commit

--- HackerRank/Python/python.py
+++ HackerRank/Python/python.py
@@ -44,6 +44,6 @@ for i in range(1, n+1):
 
 
 # List Comprehensions
-answer = [[i,j,k] for i in range(x) for j in range(y) for k in range(z) if ((i+j+k)!=n)]
+answer = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if ((i + j + k) != n)]
 print(answer)
 
